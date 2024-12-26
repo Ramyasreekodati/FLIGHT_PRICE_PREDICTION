2a667a7d8f1136306d7cc948cff6522d44ee0ba5
Ramyasreekodati <ramyasreekodati@gmail.com>
2024-12-26 13:30:15 +0530

    initial commit

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def logout():
 
 # Main function to run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080 , debug=True)
